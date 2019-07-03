b90cac6de95909de31777bc24a207b8cdd0df0df
Hugo Saracino <hugo.saracino@gmail.com>
2019-07-03 13:50:08 +0200

    :construction: wip

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,5 @@
 import { interval } from 'rxjs';
-import { map, filter } from 'rxjs/operators';
+import { map, filter, tap } from 'rxjs/operators';
 import { initializeElmApp, dispatch, sendTick } from './elm';
 
 const node = document.querySelector('#elm-root');
@@ -8,11 +8,10 @@ const app = initializeElmApp(node, 'Hello world! o/');
 
 app.ports.command.subscribe(dispatch);
 
-const seconds = interval(1000);
-
-seconds
+interval(100)
   .pipe(
     map(n => n * 10),
+    tap(console.log.bind(this)),
     filter(n => n % 100 === 0),
   )
   .subscribe(n => sendTick(app, n));
