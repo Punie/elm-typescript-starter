c17ae8b7443be57ba79ea8f6f5013c28e75998b3
Hugo Saracino <hugo.saracino@gmail.com>
2019-07-12 13:34:56 +0200

    ⚗ Code-splitting with dynamic imports
    Early attempts to make code-splitting work with dynamic imports for RxJS broke tree-shaking which resulted on a way too large bundle size. This was solved by introducing a vendor module (rxjs.ts) that just reexports needed symbols and then perform the dynamic import on this particular module.

--- src/lib/elm.ts
+++ src/lib/elm.ts
@@ -5,23 +5,19 @@ export function initializeElmApp(node: Element | null, flags: Flags): App {
   return Elm.Main.init({ node, flags });
 }
 
-function log(value: string): void {
-  console.log(value);
-}
-
-function store(value: number): void {
-  console.log(`Storing value: ${value}`);
-}
-
-export function dispatch(action: Action): void {
-  switch (action.kind) {
-    case 'log':
-      return log(action.value);
-    case 'store':
-      return store(action.value);
-    default:
-      return assertNever(action);
-  }
+export function dispatch(app: App) {
+  return function (action: Action) {
+    switch (action.kind) {
+      case 'load':
+        return load(app);
+      case 'log':
+        return log(action.value);
+      case 'store':
+        return store(action.value);
+      default:
+        return assertNever(action);
+    }
+  };
 }
 
 export function sendTick(app: App, tick: number): void {
@@ -31,4 +27,24 @@ export function sendTick(app: App, tick: number): void {
   };
 
   app.ports.notification.send(tickMsg);
+}
+
+async function load(app: App) {
+  const { interval, filter, map, tap } = await import('./rxjs');
+
+  interval(100)
+    .pipe(
+      map(n => n * 10),
+      tap(n => console.log(n)),
+      filter(n => n % 100 === 0),
+    )
+    .subscribe(n => sendTick(app, n));
+}
+
+function log(value: string) {
+  console.log(value);
+}
+
+function store(value: number) {
+  console.log(`Storing value: ${value}`);
 }
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,17 +1,6 @@
-import { interval } from 'rxjs';
-import { map, filter, tap } from 'rxjs/operators';
-import { initializeElmApp, dispatch, sendTick } from './elm';
+import { initializeElmApp, dispatch } from './elm';
 
 const node = document.querySelector('#elm-root');
-
 const app = initializeElmApp(node, 'Hello world! o/');
 
-app.ports.command.subscribe(dispatch);
-
-interval(100)
-  .pipe(
-    map(n => n * 10),
-    tap(console.log.bind(this)),
-    filter(n => n % 100 === 0),
-  )
-  .subscribe(n => sendTick(app, n));
+app.ports.command.subscribe(dispatch(app));
