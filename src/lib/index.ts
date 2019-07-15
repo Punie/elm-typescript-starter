c6d798cbad9e6efc06fb8071fa4f53385ef13847
Hugo Saracino <hugo.saracino@gmail.com>
2019-07-15 10:47:12 +0200

    :art: Handle dispatching of commands in elm module

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,4 +3,4 @@ import { initializeElmApp, dispatch } from './elm';
 const node = document.querySelector('#elm-root');
 const app = initializeElmApp(node, 'Hello world! o/');
 
-app.ports.command.subscribe(dispatch(app));
+dispatch(app);
