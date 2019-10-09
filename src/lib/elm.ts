e10010f1b03f47f662f929713ab25897bcd96a59
Hugo Saracino <zooey.teal@gmail.com>
2019-10-09 02:39:56 +0200

    🚨 Fix linter warnings

--- src/lib/elm.ts
+++ src/lib/elm.ts
@@ -5,7 +5,36 @@ export function initializeElmApp(node: Element | null, flags: Flags): App {
   return Elm.Main.init({ node, flags });
 }
 
-export function dispatch(app: App) {
+export function sendTick(app: App, tick: number): void {
+  const tickMsg: TickMessage = {
+    kind: 'tick',
+    value: tick,
+  };
+
+  app.ports.notification.send(tickMsg);
+}
+
+async function load(app: App): Promise<void> {
+  const { interval, filter, map, tap } = await import('./rxjs');
+
+  interval(100)
+    .pipe(
+      map(n => n * 10),
+      tap(n => console.log(n)),
+      filter(n => n % 100 === 0),
+    )
+    .subscribe(n => sendTick(app, n));
+}
+
+function log(value: string): void {
+  console.log(value);
+}
+
+function store(value: number): void {
+  console.log(`Storing value: ${value}`);
+}
+
+export function dispatch(app: App): void {
   app.ports.command.subscribe((action) => {
     switch (action.kind) {
       case 'load':
@@ -18,33 +47,4 @@ export function dispatch(app: App) {
         return assertNever(action);
     }
   });
-}
-
-export function sendTick(app: App, tick: number): void {
-  const tickMsg: TickMessage = {
-    kind: 'tick',
-    value: tick,
-  };
-
-  app.ports.notification.send(tickMsg);
-}
-
-async function load(app: App) {
-  const { interval, filter, map, tap } = await import('./rxjs');
-
-  interval(100)
-    .pipe(
-      map(n => n * 10),
-      tap(n => console.log(n)),
-      filter(n => n % 100 === 0),
-    )
-    .subscribe(n => sendTick(app, n));
-}
-
-function log(value: string) {
-  console.log(value);
-}
-
-function store(value: number) {
-  console.log(`Storing value: ${value}`);
 }
